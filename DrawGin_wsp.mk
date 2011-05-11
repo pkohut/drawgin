c0439784b64eea73c808a4ad9ec809fb17f8b578
Paul Kohut <pkohut2@gmail.com>
2011-05-11 10:17:37 -0700

    Removed some non-project related files

--- DrawGin_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ DrawGin - Debug ]----------
-	@cd "DrawGin" && "$(MAKE)" -f "DrawGin.mk"
-clean:
-	@echo ----------Cleaning project:[ DrawGin - Debug ]----------
-	@cd "DrawGin" && "$(MAKE)" -f "DrawGin.mk" clean
