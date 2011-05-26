fd3d887da6e76c344116977b87d12dbd5369c774
Paul Kohut <pkohut2@gmail.com>
2011-05-25 19:19:07 -0700

    add precompiled cpp and h files for MSVC

---
+++ DrawGin/src/precompiled.h
@@ -0,0 +1,15 @@
+#ifndef __PRECOMPILED_H__
+#define __PRECOMPILED_H__
+
+#include <algorithm>
+#include <assert.h>
+#include <fstream>
+#include <functional>
+#include <set>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+
+#endif
